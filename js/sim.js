de1ae8ebf0472be74bdd838ec3dddb5a825ba8f7
zhaoyuheng200 <neil.leopard@gmail.com>
2015-12-18 17:40:22 -0500

    update in index.html, sim.js add grayscale icon sprite, mastery background update style css

--- js/sim.js
+++ js/sim.js
@@ -1,5 +1,26 @@
 //Global
 var MAX_POINTS = 30;
 var POINTS_REQUIRED = [0,5,6,11,12,17];
-var POINTS_STYLES =[
-]
+var POINTS_STYLES = {
+  width: 64,
+  height: 64,
+  normal: {
+  },
+  key:{
+  }
+}
+var treeNames = ["Ferocity","Cunning","Resolve"];
+var paintSim = function(){
+  for (var tree = 0; tree < 3; tree++){
+    for (var level =0; level< masteryData[tree].length; level++){
+      var paintedLevel = paintLevel(tree,level);
+      var tier = masteryData[tree][level];
+      for (var icon=0; icon< tier.length; icon++){
+        paintIcon(paintedLevel,tier
+      }
+    }
+  }
+}
+
+var paintLevel = function(tree,level){
+}
